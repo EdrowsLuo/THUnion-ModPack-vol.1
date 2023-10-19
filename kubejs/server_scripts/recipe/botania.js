37f6a2e9c58adb0453e0139e2c3579b4aca38a52
EdrowsLuo <793971297@qq.com>
2023-10-19 18:43:58 +0800

    将一些铁质工具的配方更改成使用魔力钢

--- kubejs/server_scripts/recipe/botania.js
+++ kubejs/server_scripts/recipe/botania.js
@@ -1,4 +1,27 @@
 ServerEvents.recipes(event => {
+    // 将一些使用铁质工具的配方改成使用魔力钢工具
+    const iron2mana = (iron, mana) => {
+        event.replaceInput(
+            [
+                {
+                    type: "minecraft:crafting_shapeless",
+                    input: iron
+                },
+                {
+                    type: "minecraft:crafting_shaped",
+                    input: iron
+                },
+            ],
+            iron,
+            mana
+        )
+    }
+
+    iron2mana(minecraft.iron_axe, botania.manasteel_axe)
+    iron2mana(minecraft.iron_pickaxe, botania.manasteel_pick)
+    iron2mana(minecraft.iron_shovel, botania.manasteel_shovel)
+    iron2mana(minecraft.iron_sword, botania.manasteel_sword)
+
     event.remove({ output: botania.orechid })
     event.remove({ output: botania.entropinnyum })
     event.recipes.botania.petal_apothecary(
