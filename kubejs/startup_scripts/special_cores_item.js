d50e60ad78ce6eae4d91d0a34ea715bc60646a70
EdrowsLuo <793971297@qq.com>
2023-10-20 23:27:08 +0800

    添加物品描述和翻译

--- kubejs/startup_scripts/special_cores_item.js
+++ kubejs/startup_scripts/special_cores_item.js
@@ -12,7 +12,13 @@ StartupEvents.registry("item", event => {
         "treasure_core",
     ]
     special_cores.forEach(core => {
-        event.create(core).texture("kubejs:item/" + core).glow(true)
+        event.create(core).texture("kubejs:item/" + core)
+            .tooltip(Component.translate(`item.kubejs.${core}.desc`))
+            .glow(true)
     });
-    event.create("uncompleted_distribution_core").texture("kubejs:item/uncompleted_distribution_core").glow(true).unstackable()
+    event.create("uncompleted_distribution_core")
+        .texture("kubejs:item/uncompleted_distribution_core")
+        .tooltip(Component.translate(`item.kubejs.uncompleted_distribution_core.desc`))
+        .glow(true)
+        .unstackable()
 })
