01be27ac824e13d5781d55b69954dbb27b23ea65
EdrowsLuo <793971297@qq.com>
2023-10-20 14:30:30 +0800

    修改简易村民配方

--- kubejs/server_scripts/recipe/easy_villager.js
+++ kubejs/server_scripts/recipe/easy_villager.js
@@ -1,5 +1,7 @@
 ServerEvents.recipes(event => {
     event.remove({ output: easy_villagers.auto_trader })
+    event.remove({ output: easy_villagers.iron_farm })
+    event.remove({ output: easy_villagers.converter })
     ban(event, [easy_villagers.farmer])
 
     event.replaceInput({ output: easy_villagers.trader }, minecraft.redstone, ars_nouveau.mob_jar)
@@ -31,5 +33,60 @@ ServerEvents.recipes(event => {
         "result": {
             "item": "easy_villagers:auto_trader"
         }
+    })
+
+    event.custom({
+      "type": "minecraft:crafting_shaped",
+      "pattern": [
+        "GCG",
+        "GLG",
+        "ISI"
+      ],
+      "key": {
+        "G": {
+          "tag": "forge:glass_panes/colorless"
+        },
+        "I": {
+          "tag": "forge:ingots/iron"
+        },
+        "S": {
+          "item": easy_villagers.trader
+        },
+        "L": {
+          "item": "minecraft:lava_bucket"
+        },
+        "C": {
+            "item": kubejs.ore_core
+        }
+      },
+      "result": {
+        "item": "easy_villagers:iron_farm"
+      }
+    })
+
+    event.custom({
+      "type": "minecraft:crafting_shaped",
+      "pattern": [
+        "GGG",
+        "GFG",
+        "ICI"
+      ],
+      "key": {
+        "G": {
+          "tag": "forge:glass_panes/colorless"
+        },
+        "I": {
+          "tag": "forge:ingots/iron"
+        },
+        "F": {
+          "item": minecraft.zombie_spawn_egg
+        },
+        "C": {
+          "item": "minecraft:mossy_cobblestone"
+        }
+      },
+      "result": {
+        "item": "easy_villagers:converter"
+      }
     })
 })
