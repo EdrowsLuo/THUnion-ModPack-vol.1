cfdaaafca759f031b5bb984d70296a34f8bf393c
EdrowsLuo <793971297@qq.com>
2023-10-20 15:57:18 +0800

    修改植物魔法魔力水晶和盖亚水晶的配方

--- kubejs/server_scripts/recipe/botania.js
+++ kubejs/server_scripts/recipe/botania.js
@@ -24,6 +24,9 @@ ServerEvents.recipes(event => {
 
     event.remove({ output: botania.orechid })
     event.remove({ output: botania.entropinnyum })
+    event.remove({ output: botania.gaia_pylon })
+    event.remove({ output: botania.mana_pylon })
+
     event.recipes.botania.petal_apothecary(
         botania.orechid,
         [
@@ -59,4 +62,68 @@ ServerEvents.recipes(event => {
             }
         ]
     )
+
+    event.custom({
+        "type": "minecraft:crafting_shaped",
+        "key": {
+            "D": {
+                "item": "botania:pixie_dust"
+            },
+            "E": {
+                "tag": "botania:elementium_ingots"
+            },
+            "P": {
+                "item": "botania:mana_pylon"
+            },
+            "T": {
+                "item": ars_nouveau.wilden_tribute
+            },
+            "N": {
+                "item": l2complements.eternium_nugget
+            },
+            "S": {
+                "item": kubejs.treasure_core
+            },
+            "L": {
+                "item": kubejs.life_core
+            }
+        },
+        "pattern": [
+            "SDL",
+            "EPE",
+            "TDN"
+        ],
+        "result": {
+            "item": "botania:gaia_pylon"
+        }
+    })
+
+    event.custom({
+        "type": "minecraft:crafting_shaped",
+        "key": {
+            "E": {
+                "tag": "botania:mana_diamond_gems"
+            },
+            "G": {
+                "item": "minecraft:gold_ingot"
+            },
+            "M": {
+                "tag": "botania:manasteel_ingots"
+            },
+            "D": {
+                "item": ars_nouveau.source_gem_block
+            },
+            "B": {
+                "item": ars_nouveau.magebloom_fiber
+            }
+        },
+        "pattern": [
+            "DGD",
+            "MEM",
+            "BGB"
+        ],
+        "result": {
+            "item": "botania:mana_pylon"
+        }
+    })
 })
